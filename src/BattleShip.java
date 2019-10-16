e76e88c43d80651decdae4d1ccb4545a48f6fa96
Sergio <sergioprogramacion19@gmail.com>
2019-10-16 19:42:46 +0200

    added comment

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class BattleShip {
 	
+	//Hola mundo
+	
 	public static final int SHIP_SYMBOL = 'S';
 	public static final int WATER_SYMBOL = 'O';
 	public static final int SUNK_SHIP_SYMBOL = 'X';
